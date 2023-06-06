8a3c939e3fbb257ba04cb23c373c4ae094f6fbd2
Seenuprime <87597017+Seenuprime@users.noreply.github.com>
2023-06-06 21:53:06 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const featuredMoviesContainer = document.getElementById("featured-movies");
+
+  // Fetch random movie data from TMDb API
+  fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=45456cc15869831c16bb0353d65283c0")
+    .then(response => response.json())
+    .then(data => {
+      // Check if the response contains movies
+      if (data.results) {
+        const movies = data.results;
+
+        // Loop through the movie data and create movie cards
+        movies.forEach(movie => {
+          const movieCard = document.createElement("div");
+          movieCard.classList.add("movie-card");
+
+          const movieImage = document.createElement("img");
+          movieImage.src = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+          movieImage.alt = movie.title;
+          movieCard.appendChild(movieImage);
+
+          const movieTitle = document.createElement("h3");
+          movieTitle.textContent = movie.title;
+          movieCard.appendChild(movieTitle);
+
+          const googleLink = document.createElement("a");
+          googleLink.href = "https://www.google.com/search?q=" + encodeURIComponent(movie.title+' imdb');
+          googleLink.target = "_blank";
+          googleLink.textContent = "Search on Google";
+          googleLink.classList.add("google-link"); // Add a CSS class for styling
+          movieCard.appendChild(googleLink);
+
+          featuredMoviesContainer.appendChild(movieCard);
+        });
+      } else {
+        console.log("No movies found.");
+      }
+    })
+    .catch(error => {
+      console.log("An error occurred while fetching movie data:", error);
+    });
+});
